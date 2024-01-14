67cf2c04976db95cd3e2b2cf47e0286c46187ec0
aliCh10 <130141511+aliCh10@users.noreply.github.com>
2024-01-14 15:27:58 +0100

    partie final js

--- script.js
+++ script.js
@@ -48,4 +48,21 @@ function populateUI() {
     movieSelect.selectedIndex = selectedMovieIndex;
   }
 }
+// Movie select event
+movieSelect.addEventListener('change', (e) => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
 
+// Seat click event
+container.addEventListener('click', (e) => {
+  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
+
+// intial count and total
+updateSelectedCount();
